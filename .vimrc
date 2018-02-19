5978b2ad52a0daa65dff1d6552fe22d89a079333
ttuche <vlvl1217@daum.net>
2018-02-19 18:55:21 +0900

    first commit

---
+++ .vimrc
@@ -0,0 +1,12 @@
+set number
+syntax on 
+set autoindent
+set cindent
+det smartindent
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set hlsearch
+set expandtab
+autocmd FileType make setlocal noexpandtab
+
---
+++ lsw/cla.c
@@ -0,0 +1,12 @@
+#include<stdioh>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i=0 ; i < argc ; i++)
+//	for(i=0; <argv[i] !=NULL ; i++) 
+		printf("argw[%d]: %s\n", i, arvg[i);
+	exit(0);
+}
